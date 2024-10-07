e7c48e5dcc35d14483be6341aa5b759653398b1b
Murilo Corso <murilofcorso@gmail.com>
2024-10-07 18:25:23 -0300

    Utilizando express-handlebars

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-// const express = require("express");
-// const app = express();
-// app.get("/", function(req, res){
-//     res.write("Hello World!");
-//     res.end();
-// })
-// app.listen(8080);
+import express from 'express';
+import { engine } from 'express-handlebars';
 
+const app = express();
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set("views", "./views");
 
+app.use(express.static("public"));
 
-const mysql = require("mysql2");
-const conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"1234",
-    database:"projetocriptografia"
+app.get("/", function(req, res){
+    res.render("index");
 });
-conn.connect(function(erro){
-    if(erro) throw erro;
-    console.log("Conexão efetuada com sucesso!")
-    var sql = "INSERT INTO senhas (usuario, senha, nota) VALUES ('murilofcorso', '12345', 'Senha do e-mail')";
-    conn.query(sql, function(erro, result){
-        if(erro) throw erro;
-        console.log("Funcionou!")
-    });
-});
+
+app.listen(8080);
+
+
+
+
+
+
+
+
+
+
+// // Importando mysql2
+// const mysql = require("mysql2");
+// // Criando conexão sql
+// const conn = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"1234",
+//     database:"projetocriptografia"
+// });
+// // Conectando / Lançando possíveis erros
+// conn.connect(function(erro){
+//     if(erro) throw erro;
+//     console.log("Conexão efetuada com sucesso!")
+
+//     // Inserindo dados no banco / Lançando possíveis erros
+//     var sql = "INSERT INTO senhas (usuario, senha, nota) VALUES ('murilofcorso', '12345', 'Senha do e-mail')";
+//     conn.query(sql, function(erro, result){
+//         if(erro) throw erro;
+//         console.log("Funcionou!")
+//     });
+// });
